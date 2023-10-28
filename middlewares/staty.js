e3b6870e8ad1c57b7cfc1d01a3ec23e3df90a24c
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-28 17:53:08 +0200

    Create staty.js

---
+++ middlewares/staty.js
@@ -0,0 +1,13 @@
+const { BOT_ID } = require("../config/secret.json");
+
+const staty = async (req, res, next) => {
+
+    if(req.headers.statyid === BOT_ID) {
+        next();
+    }
+    else {
+        res.status(403).json({ message: "Not authorized" });
+    }
+}
+
+module.exports = staty;
