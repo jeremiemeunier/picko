7529c82626374376fd864963e8cb8f29ba6a63cc
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-28 17:36:29 +0200

    Create api.js

---
+++ functions/api.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const RateLimit = require('express-rate-limit');
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const { PORT, MONGODB_URL } = require('../config/secret.json');
+
+const { logger } = require('../functions/logger');
+
+const api = () => {
+    const limiter = RateLimit({
+        windowMs: 15 * 60 * 1000,
+        max: 100,
+    });
+
+    app.use(limiter);
+    app.use(express.json());
+    app.use(cors());
+
+    // BDD
+
+    try {
+        mongoose.connect(MONGODB_URL);
+    }
+    catch(error) { logger(error); }
+
+    try {
+        // API
+        // const infractionRoute = require('./routes/infraction');
+
+        // app.use(infractionRoute);
+
+        app.get("/", (req, res) => {
+            res.status(200).json({ message: "Bienvenue sur le backend de Staty" });
+        });
+
+        // Route 404
+        app.all("*", (req, res) => {
+            res.status(404).json({ message: "This route do not exist" });
+        });
+        
+        app.listen(PORT, () => {
+            logger(`🚀 | API Server : Started on port ${PORT}`);
+        });
+    }
+    catch(error) {
+        logger(`API Server : 🔴 | An error occured on api : ${error}`);
+    }
+}
+
+module.exports = { api }
