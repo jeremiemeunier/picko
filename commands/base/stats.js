07bca37840d559525643fcdcde543c48c908cb7c
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-29 15:05:02 +0100

    Update stats.js

--- commands/base/stats.js
+++ commands/base/stats.js
@@ -2,7 +2,8 @@ const commands =
 {
     name: "stats",
     description: "Return statistics of last 24 hours for api",
-    default_member_permissions: 0
+    default_member_permissions: 0,
+    database: true
 };
 
 module.exports = {
