0b45168fb346d0a259efccd4027bb6a5effe7fd0
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-29 12:25:03 +0100

    Create stats.js

---
+++ commands/base/stats.js
@@ -0,0 +1,10 @@
+const commands =
+{
+    name: "stats",
+    description: "Return statistics of last 24 hours for api",
+    default_member_permissions: 0
+};
+
+module.exports = {
+    data: commands
+}
