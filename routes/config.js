41fb2379a0e124496c2f2fdd7301fb2c720fb7d1
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-18 19:34:17 +0100

    Update config.js

--- routes/config.js
+++ routes/config.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const Ping = require('../models/Config');
+const Config = require('../models/Config');
 const staty = require('../middlewares/staty');
 
 const { logger } = require('../functions/logger');
 
+router.post('/setup', staty, async (req, res) => {
+  const { guild, channel, role } = req.body;
 
+  try {
+    const newConfig = new Config({
+      guild_id: guild,
+      role: role,
+      channel: channel
+    });
+    await newConfig.save();
+
+    res.status(200).json({ message: "New configuration registred" });
+  }
+  catch(error) { logger(`🔴 [api:setup:register] ${error}`); }
+});
 
 module.exports = router;
