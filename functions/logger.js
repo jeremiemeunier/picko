01fe335a0b83a02fa3375ddf7b4407dbe626ec8b
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-18 18:54:45 +0100

    Update logger.js

--- functions/logger.js
+++ functions/logger.js
@@ -10,14 +10,14 @@ const loggerBoot = async (client, channel) => {
 const composeTime = () => {
     const now = new Date();
 
-    const day = now.getDay().length < 2 ? `0${now.getDay()}` : now.getDay();
-    const month = now.getMonth().length < 2 ? `0${now.getMonth()}` : now.getMonth();
+    const day = now.getDay().toString().length < 2 ? `0${now.getDay()}` : now.getDay();
+    const month = now.getMonth().toString().length < 2 ? `0${now.getMonth()}` : now.getMonth();
     const year = now.getFullYear();
 
-    const hours = now.getHours().length < 2 ? `0${now.getHours()}` : now.getHours();
-    const minutes = now.getMinutes().length < 2 ? `0${now.getMinutes()}` : now.getMinutes();
-    const seconds = now.getSeconds().length < 2 ? `0${now.getSeconds()}` : now.getSeconds();
-    const miliseconds = now.getMilliseconds().length < 3 ? `0${now.getMilliseconds()}` : now.getMilliseconds();
+    const hours = now.getHours().toString().length < 2 ? `0${now.getHours()}` : now.getHours();
+    const minutes = now.getMinutes().toString().length < 2 ? `0${now.getMinutes()}` : now.getMinutes();
+    const seconds = now.getSeconds().toString().length < 2 ? `0${now.getSeconds()}` : now.getSeconds();
+    const miliseconds = now.getMilliseconds().toString().length < 3 ? `0${now.getMilliseconds()}` : now.getMilliseconds();
 
     return `[${day}/${month}/${year} ${hours}:${minutes}:${seconds}.${miliseconds}]`;
 }
