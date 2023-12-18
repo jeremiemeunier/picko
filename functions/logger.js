55f6a1779158eb7452496daa3de824a8ce077776
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-18 19:34:10 +0100

    Update logger.js

--- functions/logger.js
+++ functions/logger.js
@@ -1,12 +1,6 @@
 const { version } = require('../package.json');
 const tag = `staty[${version}] `;
 
-let channelConsole, client;
-
-const loggerBoot = async (client, channel) => {
-    channelConsole = channel;
-}
-
 const composeTime = () => {
     const now = new Date();
 
@@ -23,30 +17,7 @@ const composeTime = () => {
 }
 
 const logger = async (content) => {
-    const now = new Date();
-
     console.log(`${composeTime()} ${tag}${content}`);
-    
-    try {
-        channelConsole.messages.fetch().then(messages => {
-            let lastLog = messages.first();
-            
-            if(lastLog !== undefined) {
-                let lastLogContent = lastLog.content.slice(0, -3);
-                let newLogContent = `${lastLogContent}\r\n${tag + content + '```'}`;
-
-                if(newLogContent.length >= 2000) {
-                    try { channelConsole.send({ content: '```' + tag + content + '```' }); }
-                    catch(error) { console.log(error); }
-                }
-                else {
-                    try { lastLog.edit(newLogContent); }
-                    catch(error) { console.log(error); }
-                }
-            }
-            else { channelConsole.send({ content: '```' + tag + content + '```' }); }
-        });
-    } catch(error) { console.log(`${logDate} ${tag}${error}`); }
 }
 
-module.exports = { logger, loggerBoot }
+module.exports = { logger }
