f672b14303a6fb8d9bdd6d6fe4795d463a6fdfc1
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-19 17:49:18 +0100

    Create starter.js

---
+++ functions/starter.js
@@ -0,0 +1,71 @@
+const { BOT_ID } = require('../config/secret.json');
+const axios = require('axios');
+const { logger } = require('./logger');
+const { statyPing } = require('./tester');
+
+const statyStarter = async (guildId, guild) => {
+
+  try {
+    const setup = await axios({
+      method: "get",
+      url: "http://localhost:3000/setup",
+      params: {
+          guild: guildId
+      },
+      headers: {
+          statyid: BOT_ID
+      }
+    });
+
+    if(setup.data.data) {
+      const { role, channel } = setup.data.data;
+      const statsChannel = guild.channels.cache.find(statsChannel => statsChannel.id === channel);
+
+      // List all guildId api
+      try {
+        const allApiRequest = await axios({
+          method: "get",
+          url: "http://localhost:3000/api/all",
+          params: {
+              guild: guildId
+          },
+          headers: {
+            statyid: BOT_ID
+          }
+        });
+
+        const allApiList = allApiRequest.data.data;
+        let pingArray = [];
+
+        // Lancement de tout les pings
+
+        allApiList.map((item, index) => {
+          statyPing(item, {
+            state: statsChannel,
+            role: role,
+            guild: guild
+          });
+
+          pingArray.push(item.api_name);
+        });
+
+        const allThreads = statsChannel.threads.cache;
+        await allThreads.map(thread => {
+          if(pingArray.indexOf(thread.name.slice(3)) < 0) {
+            try { thread.delete(); }
+            catch(error) { logger(`🔴 | ${error}`); }
+          }
+        });
+
+      }
+      catch(error) {
+        logger(`🔴 [starter:get_all_api] ${error}`);
+      }
+    }
+  }
+  catch(error) {
+    logger(`🔴 [starter:get_setup] ${error}`);
+  }
+}
+
+module.exports = { statyStarter }
