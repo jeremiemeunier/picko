0bfc505513f1ef9f810c40d643673a2664d843f5
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-19 17:49:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,77 +6,11 @@ const client = new Client({
     partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
-const { dateParser } = require('./functions/dateParser');
 const { logger } = require('./functions/logger');
-const { statyPing } = require('./functions/tester');
 const { api } = require('./functions/api');
 const { commandRegisterInit, commandRegister } = require('./functions/commandsRegister');
 const { interactionCreateEventInit } = require('./events/interactionCreateEvent');
-
-const statyStarter = async (guildId, guild) => {
-
-    try {
-        const setup = await axios({
-            method: "get",
-            url: "http://localhost:3000/setup",
-            params: {
-                guild: guildId
-            },
-            headers: {
-                statyid: BOT_ID
-            }
-        });
-
-        if(setup.data.data) {
-            const { role, channel } = setup.data.data;
-            const statsChannel = guild.channels.cache.find(statsChannel => statsChannel.id === channel);
-
-            // List all guildId api
-            try {
-                const allApiRequest = await axios({
-                    method: "get",
-                    url: "http://localhost:3000/api/all",
-                    params: {
-                        guild: guildId
-                    },
-                    headers: {
-                        statyid: BOT_ID
-                    }
-                });
-
-                const allApiList = allApiRequest.data.data;
-                let pingArray = [];
-
-                // Lancement de tout les pings
-
-                allApiList.map((item, index) => {
-                    statyPing(item, {
-                        state: statsChannel,
-                        role: role,
-                        guild: guild
-                    });
-
-                    pingArray.push(item.api_name);
-                });
-
-                const allThreads = statsChannel.threads.cache;
-                await allThreads.map(thread => {
-                    if(pingArray.indexOf(thread.name.slice(3)) < 0) {
-                        try { thread.delete(); }
-                        catch(error) { logger(`🔴 | ${error}`); }
-                    }
-                });
-
-            }
-            catch(error) {
-                logger(`🔴 [starter:get_all_api] ${error}`);
-            }
-        }
-    }
-    catch(error) {
-        logger(`🔴 [starter:get_setup] ${error}`);
-    }
-}
+const { statyStarter } = require('./functions/starter');
 
 const booter = () => {
     const allGuilds = client.guilds.cache;
