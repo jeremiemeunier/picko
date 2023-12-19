f69801b687f9651a20c4524819bfb810a7957b88
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-19 11:20:43 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@ router.post('/api/add', staty, async (req, res) => {
   try {
     const newApi = new Api({
       guild_id: guild,
-      role: role,
+      role: role || null,
       api_name: name,
       api_adress: adress
     });
@@ -21,6 +21,25 @@ router.post('/api/add', staty, async (req, res) => {
   }
   catch(error) {
     logger(`🔴 [api:add_api:register] ${error}`);
+    res.status(400);
+  }
+});
+
+router.get('/api/all', staty, async (req, res) => {
+  const { guild } = req.query;
+
+  try {
+    const allApi = await Api.find({ guild_id: guild });
+
+    if(!allApi) {
+      res.status(404).json({ message: "No api find for this guild" });
+    }
+    else {
+      res.status(200).json({ message: "Api find", data: allApi });
+    }
+  }
+  catch(error) {
+    logger(`🔴 [api:add_api:get_all] ${error}`);
     res.status(400);
   }
 });
