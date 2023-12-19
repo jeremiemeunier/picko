beefc13b91cd0235beec2363bf679feb338eb47b
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-19 19:04:46 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -44,13 +44,29 @@ router.get('/api/all', staty, async (req, res) => {
   }
 });
 
+router.get('/api/id/:id', staty, async (req, res) => {
+  try {
+    const allApi = await Api.findById({ _id: id });
+
+    if(!allApi) {
+      res.status(404).json({ message: "No api find for this guild" });
+    }
+    else {
+      res.status(200).json({ message: "Api find", data: allApi });
+    }
+  }
+  catch(error) {
+    logger(`🔴 [api:add_api:get_all] ${error}`);
+    res.status(400);
+  }
+});
+
 router.delete('/api/remove', staty, async (req, res) => {
   const { id } = req.query;
 
   try {
-    const allApi = await Api.findByIdAndRemove({ _id: id });
-
-    res.status(200).json({ message: "Api removed" });
+    await Api.findByIdAndRemove({ _id: id });
+    res.status(200).json({ message: "Api removed", data: id });
   }
   catch(error) {
     logger(`🔴 [api:add_api:remove] ${error}`);
