6652bbc1b6b1e1c844f39972da73cc3694b1c4bf
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-19 11:20:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-const { BOT_TOKEN, PORT } = require('./config/secret.json');
+const { BOT_TOKEN, BOT_ID } = require('./config/secret.json');
 const { options, channels, database } = require('./config/global.json');
 const { version, name } = require('./package.json');
+const axios = require('axios');
 const { color } = options;
 const apiSettings = JSON.parse(fs.readFileSync('config/api.json'));
 const { Client, EmbedBuilder, GatewayIntentBits, Partials } = require('discord.js');
@@ -17,56 +18,109 @@ const { api } = require('./functions/api');
 const { commandRegisterInit } = require('./functions/commandsRegister');
 const { interactionCreateEventInit } = require('./events/interactionCreateEvent');
 
-const booter = async () => {
-    const channelConsole  = client.channels.cache.find(channel => channel.name === channels.console);
-    const channelDebug    = client.channels.cache.find(channel => channel.name === channels.debug);
-    const channelState    = client.channels.cache.find(channel => channel.name === channels.state);
+const statyStarter = async (guildId, guild) => {
 
-    let pingArray = [];
-
-	try {
-        let bootEmbed = new EmbedBuilder()
-            .setColor(color)
-            .setDescription(name)
-            .addFields(
-                { name: 'Date starting', value: dateParser(new Date()), inline: true },
-                { name: 'Version', value: version.toString(), inline: true },
-                { name: 'API', value: database.toString(), inline: true }
-            )
-            .setTimestamp()
-            .setFooter({ text: `Version ${version}`, });
-	    channelDebug.send({ embeds: [bootEmbed] });
-        logger('😊 [bot:start] Hello here !');
-
-        commandRegisterInit(client);
-        interactionCreateEventInit(client);
-
-        if(database) {
-            logger('🟢 [database:use] Using database for statistics');
-            api();
-            logger(`🟢 [api:launch] Lauching API on port 3000`);
-        }
-        else { logger('🔴 [database:use] Dont use database for statistics'); }
-
-        // Lancement de tout les pings
-        for(let i = 0;i < apiSettings.api.map(x => x).length;i++) {
-            statyPing(apiSettings.api[i], {
-                state: channelState,
-                debug: channelDebug,
-                console: channelConsole
-            });
-            pingArray.push(apiSettings.api[i].name);
-        }
-
-        const allThreads = channelState.threads.cache;
-        await allThreads.map(thread => {
-            if(pingArray.indexOf(thread.name.slice(3)) < 0) {
-                try { thread.delete(); }
-                catch(error) { logger(`🔴 | ${error}`); }
+    try {
+        const setup = await axios({
+            method: "get",
+            url: "http://localhost:3000/setup",
+            params: {
+                guild: guildId
+            },
+            headers: {
+                statyid: BOT_ID
             }
         });
+
+        if(setup.data.data) {
+            const { role, channel } = setup.data.data;
+            const statsChannel = guild.channels.cache.find(statsChannel => statsChannel.id === channel);
+            const statsRole = guild.roles.cache.find(statsRole => statsRole.id === role);
+
+            // List all guildId api
+            try {
+                const allApi = await axios({
+                    method: "get",
+                    url: "http://localhost:3000/api/all",
+                    params: {
+                        guild: guildId
+                    },
+                    headers: {
+                        statyid: BOT_ID
+                    }
+                });
+
+                console.log(allApi.data.data);
+            }
+            catch(error) {
+                logger(`🔴 [starter:get_all_api] ${error}`);
+            }
+        }
     }
-    catch(error) { logger(`🔴 [staty:global] ${error}`); }
+    catch(error) {
+        logger(`🔴 [starter:get_setup] ${error}`);
+    }
+}
+
+const booter = async () => {
+    const allGuilds = client.guilds.cache;
+
+    logger('🟢 [database:use] Using database for statistics');
+    api();
+    logger(`🟢 [api:launch] Lauching API on port 3000`);
+
+    allGuilds.map((item, index) => {
+        statyStarter(item.id, item);
+    });
+
+
+    // const channelState    = client.channels.cache.find(channel => channel.name === channels.state);
+
+    // let pingArray = [];
+
+	// try {
+    //     let bootEmbed = new EmbedBuilder()
+    //         .setColor(color)
+    //         .setDescription(name)
+    //         .addFields(
+    //             { name: 'Date starting', value: dateParser(new Date()), inline: true },
+    //             { name: 'Version', value: version.toString(), inline: true },
+    //             { name: 'API', value: database.toString(), inline: true }
+    //         )
+    //         .setTimestamp()
+    //         .setFooter({ text: `Version ${version}`, });
+	//     channelDebug.send({ embeds: [bootEmbed] });
+    //     logger('😊 [bot:start] Hello here !');
+
+    //     commandRegisterInit(client);
+    //     interactionCreateEventInit(client);
+
+    //     if(database) {
+    //         logger('🟢 [database:use] Using database for statistics');
+    //         api();
+    //         logger(`🟢 [api:launch] Lauching API on port 3000`);
+    //     }
+    //     else { logger('🔴 [database:use] Dont use database for statistics'); }
+
+    //     // Lancement de tout les pings
+    //     for(let i = 0;i < apiSettings.api.map(x => x).length;i++) {
+    //         statyPing(apiSettings.api[i], {
+    //             state: channelState,
+    //             debug: channelDebug,
+    //             console: channelConsole
+    //         });
+    //         pingArray.push(apiSettings.api[i].name);
+    //     }
+
+    //     const allThreads = channelState.threads.cache;
+    //     await allThreads.map(thread => {
+    //         if(pingArray.indexOf(thread.name.slice(3)) < 0) {
+    //             try { thread.delete(); }
+    //             catch(error) { logger(`🔴 | ${error}`); }
+    //         }
+    //     });
+    // }
+    // catch(error) { logger(`🔴 [staty:global] ${error}`); }
 }
 
 client.on('ready', () => { booter(); });
