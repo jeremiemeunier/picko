3775cd846fd44ced263cb8420f8890a3465b8920
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-03-05 20:01:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const { Client, GatewayIntentBits, Partials, Events } = require("discord.js");
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildVoiceStates,
-  ],
-  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
-});
-
-const { logger } = require("./functions/logger");
-const { api } = require("./functions/api");
-const {
-  commandRegisterInit,
-  commandRegister,
-} = require("./functions/commandsRegister");
-const {
-  interactionCreateEventInit,
-} = require("./events/interactionCreateEvent");
-const { statyStarter } = require("./functions/starter");
-
-const booter = () => {
-  const allGuilds = client.guilds.cache;
-
-  logger("🟢 [database:use] Using database for statistics");
-  api();
-  logger(`🟢 [api:launch] Lauching API on port 3000`);
-
-  commandRegisterInit(client);
-  interactionCreateEventInit(client);
-
-  allGuilds.map((item) => {
-    statyStarter(item.id, item);
+try {
+  const BOT_TOKEN = process.env.BOT_TOKEN;
+  const { Client, GatewayIntentBits, Partials, Events } = require("discord.js");
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.GuildVoiceStates,
+    ],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
   });
 
-  client.on(Events.GuildCreate, (guild) => {
-    logger(`🚀 [staty:on_join] Join a new server : ${guild.id} ${guild.name}`);
-    commandRegister(guild.id);
-    statyStarter(guild.id, guild);
-  });
-};
+  const { logger } = require("./functions/logger");
+  const { api } = require("./functions/api");
+  const {
+    commandRegisterInit,
+    commandRegister,
+  } = require("./functions/commandsRegister");
+  const {
+    interactionCreateEventInit,
+  } = require("./events/interactionCreateEvent");
+  const { statyStarter } = require("./functions/starter");
 
-client.on("ready", () => {
-  booter();
-});
-client.login(BOT_TOKEN);
+  const booter = () => {
+    const allGuilds = client.guilds.cache;
+
+    logger("🟢 [database:use] Using database for statistics");
+    api();
+    logger(`🟢 [api:launch] Lauching API on port 3000`);
+
+    commandRegisterInit(client);
+    interactionCreateEventInit(client);
+
+    allGuilds.map((item) => {
+      statyStarter(item.id, item);
+    });
+
+    client.on(Events.GuildCreate, (guild) => {
+      logger(
+        `🚀 [staty:on_join] Join a new server : ${guild.id} ${guild.name}`
+      );
+      commandRegister(guild.id);
+      statyStarter(guild.id, guild);
+    });
+  };
+
+  client.on("ready", () => {
+    booter();
+  });
+  client.login(BOT_TOKEN);
+} catch (error) {
+  console.log(error);
+}
