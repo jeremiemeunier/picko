3d445f365b06b5623842c13075236fb9e3cce3ed
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-03-21 23:45:55 +0100

    Update tester.js

--- functions/tester.js
+++ functions/tester.js
@@ -1,246 +1,150 @@
-const { options } = require("../config/global.json");
-const BOT_ID = process.env.BOT_ID;
-const { color, wait } = options;
-const { EmbedBuilder, ChannelType, time } = require("discord.js");
-const axios = require("axios");
+import { EmbedBuilder, ChannelType, time } from "discord.js";
+import axios from "axios";
+import { logger } from "./logger";
 
-const { logger } = require("./logger");
-
-const statyPing = async (apiData, params) => {
-  const { state, role, guild } = params;
+export const statyPing = async (apiData, params) => {
+  const { state, role, guild, wait } = params;
   const waitingTime = wait >= 300000 ? wait : 300000;
 
   try {
-    let lastPingState = 0;
-    let apiIsDown = undefined;
-    let apiIsUp = undefined;
+    // on error create a thread to details error and ping user
+    // on start and success nothings
 
-    const pingInit = new EmbedBuilder()
-      .setColor(color)
-      .setDescription(`🚀 Staty now monitoring api`);
-    const pingEmbed = new EmbedBuilder()
-      .setColor(color)
-      .setDescription(time(new Date()));
-    const initThreadEmbed = new EmbedBuilder()
-      .setColor(color)
-      .setTitle(apiData.api_name)
-      .setDescription(`You are added to this thread to monitoring api`);
-    let pingThread;
+    let lastApiState = 0;
+    let threadPing;
+    let apiFetchedData;
 
-    try {
-      if (
-        state.threads.cache.find((thread) =>
-          thread.name.endsWith(apiData.api_name)
-        )
-      ) {
-        pingThread = state.threads.cache.find((thread) =>
-          thread.name.endsWith(apiData.api_name)
-        );
-        pingThread.setName(`🚀 ${apiData.api_name}`);
-      } else {
-        pingThread = await state.threads.create({
-          name: `🚀 ${apiData.api_name}`,
-          autoArchiveDuration: 60,
-          reason: `Dedicated thread for pinging api ${apiData.api_name}`,
-          type: ChannelType.PrivateThread,
+    const pingInterval = setInterval(async () => {
+      const now = new Date();
+
+      try {
+        // check if this api has removed from watching
+        const apiCheck = await axios.get("/api/id", {
+          params: {
+            id: apiData._id,
+          },
+          headers: {
+            statyid: process.env.BOT_ID,
+          },
         });
-      }
-    } catch (error) {
-      logger(`🔴 [ping:find_thread] ${error}`);
-    }
 
-    try {
-      const message = await pingThread.send({
-        embeds: [initThreadEmbed],
-        content: `<@&${apiData.role === null ? role : apiData.role}>`,
-      });
-      const messagePingInit = await pingThread.send({
-        embeds: [pingInit, pingEmbed],
-      });
-    } catch (error) {
-      logger(`🔴 [ping:create_message] ${error}`);
-    }
+        apiFetchedData = apiCheck.data;
 
-    try {
-      const pingInterval = setInterval(
-        async () => {
-          const now = new Date();
+        try {
+          // now check health of api endpoint
+          await axios({
+            method: "get",
+            url: apiFetchedData.api_adress,
+          });
 
+          // api respond with an success code
+          // register the ping to indicate an up api
           try {
-            const isPinging = await axios.get("/api/id", {
-              params: {
-                id: apiData._id,
+            await axios.post(
+              "/ping",
+              {
+                name: apiFetchedData.api_name,
+                state: true,
+                date: now,
+                api: apiFetchedData._id,
               },
-              headers: {
-                statyid: BOT_ID,
-              },
-            });
+              {
+                headers: {
+                  statyid: process.env.BOT_ID,
+                },
+              }
+            );
+          } catch (error) {
+            logger(`🔴 [ping:database:register] ${error}`);
+          }
 
-            try {
-              const request = await axios({
-                method: "get",
-                baseUrl: apiData.api_adress,
-              });
+          if (lastApiState < 0) {
+            if (
+              state.threads.cache.find((thread) =>
+                thread.name.endsWith(apiFetchedData.api_name)
+              )
+            ) {
+              /*
+               * if last state is down and if exist a thread
+               * we remove thread
+               */
+              const failureThread = state.threads.cache.find((thread) =>
+                thread.name.endsWith(apiFetchedData.api_name)
+              );
 
               try {
-                await axios.post(
-                  "/ping",
-                  {
-                    name: apiData.api_name,
-                    state: true,
-                    date: now,
-                    guild: guild.id,
-                    api: apiData._id,
-                  },
-                  {
-                    headers: {
-                      statyid: BOT_ID,
-                    },
-                  }
-                );
+                await failureThread.delete();
               } catch (error) {
-                logger(`🔴 [ping:database:register] ${error}`);
-              }
-
-              if (lastPingState > 1) {
-                try {
-                  pingThread.setName(`🟠 ${apiData.api_name}`);
-                  apiIsUp = await pingThread.send({
-                    content: `@here API is now UP !`,
-                  });
-
-                  const pingInit = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(`🟠 ${apiData.api_adress}`);
-                  const pingEmbed = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(time(new Date()));
-                  messagePingInit.edit({ embeds: [pingInit, pingEmbed] });
-                  lastPingState = 1;
-
-                  if (apiIsDown !== undefined) {
-                    apiIsDown.delete();
-                    apiIsDown = undefined;
-                  }
-                } catch (error) {
-                  logger(`🔴 [ping:set_is_reup] ${error}`);
-                }
-              } else {
-                try {
-                  if (apiIsUp !== undefined || lastPingState === 0) {
-                    pingThread.setName(`🟢 ${apiData.api_name}`);
-                  }
-
-                  const pingInit = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(`🟢 ${apiData.api_adress}`);
-                  const pingEmbed = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(time(new Date()));
-                  messagePingInit.edit({ embeds: [pingInit, pingEmbed] });
-                  lastPingState = 1;
-
-                  if (apiIsUp !== undefined) {
-                    apiIsUp.delete();
-                    apiIsUp = undefined;
-                  }
-                } catch (error) {
-                  logger(`🔴 [ping:set_is_up] ${error}`);
-                }
-              }
-            } catch (error) {
-              try {
-                await axios({
-                  method: "post",
-                  url: `http://localhost:3000/ping`,
-                  data: {
-                    name: apiData.api_name,
-                    state: false,
-                    date: now,
-                    api: apiData._id,
-                  },
-                  headers: {
-                    statyid: BOT_ID,
-                  },
-                });
-              } catch (error) {
-                logger(`🔴 [ping:database:register] ${error}`);
-              }
-
-              if (lastPingState === 2) {
-                try {
-                  pingThread.setName(`🔥 ${apiData.api_name}`);
-
-                  const pingInit = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(`🔥 ${apiData.api_adress}`);
-                  const pingEmbed = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(time(new Date()));
-                  messagePingInit.edit({ embeds: [pingInit, pingEmbed] });
-                  lastPingState = 3;
-                } catch (error) {
-                  logger(`🔴 [ping:set_is_down_2] ${error}`);
-                }
-              } else if (lastPingState === 3) {
-                try {
-                  pingThread.setName(`⚫ ${apiData.api_name}`);
-
-                  const pingInit = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(`⚫ ${apiData.api_adress}`);
-                  const pingEmbed = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(time(new Date()));
-                  messagePingInit.edit({ embeds: [pingInit, pingEmbed] });
-                  lastPingState = 3;
-                } catch (error) {
-                  logger(`🔴 [ping:set_is_down_3] ${error}`);
-                }
-              } else {
-                try {
-                  pingThread.setName(`🔴 ${apiData.api_name}`);
-
-                  const pingInit = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(`🔴 ${apiData.api_adress}`);
-                  const pingEmbed = new EmbedBuilder()
-                    .setColor(color)
-                    .setDescription(time(new Date()));
-                  messagePingInit.edit({ embeds: [pingInit, pingEmbed] });
-                  lastPingState = 2;
-
-                  apiIsDown = await pingThread.send({
-                    content: `@here API is now down !`,
-                  });
-                  const downConsole = new EmbedBuilder()
-                    .setColor(color)
-                    .setTitle(`API is down !`)
-                    .setDescription(
-                      `Find here log for latest ping\r\n\`\`\`An error occured on API ping for ${apiData.adress} → ${error.response.status} [${error.response.statusText}]\`\`\``
-                    );
-                  pingThread.send({ embeds: [downConsole] });
-                } catch (error) {
-                  logger(`🔴 [ping:set_is_down_1] ${error}`);
-                }
+                logger(`🔴 [ping:remove_failure_thread] ${error}`);
               }
             }
-          } catch (error) {
-            logger(`🔴 [ping:inactive_ping] ${error}`);
-            clearInterval(pingInterval);
           }
-        },
-        waitingTime,
-        () => {
-          logger(`🔴 [stop:interval] api id ${apiData._id}`);
+        } catch (error) {
+          console.log(error);
+          // api respond with an error code
+          // register the ping to indicate a down api
+          try {
+            await axios.post(
+              "/ping",
+              {
+                name: apiFetchedData.api_name,
+                state: false,
+                date: now,
+                api: apiFetchedData._id,
+              },
+              {
+                headers: {
+                  statyid: process.env.BOT_ID,
+                },
+              }
+            );
+          } catch (error) {
+            logger(`🔴 [ping:database:register] ${error}`);
+          }
+
+          // check previous state of api
+          if (lastApiState === 1 || lastApiState === 0) {
+            /*
+             * last ping is start ping or up
+             * create a thread to ping and indicate error
+             * send error message for console error
+             */
+            try {
+              threadPing = await state.threads.create({
+                name: `🔴 ${apiFetchedData.api_name}`,
+                autoArchiveDuration: 60,
+                reason: `Dedicated thread for pinging api ${apiFetchedData.api_name}`,
+                type: ChannelType.PrivateThread,
+              });
+
+              // Creating embed and send
+              const pingInit = new EmbedBuilder()
+                .setColor(parseInt("E9D84E", 16))
+                .setDescription(`🔴 ${apiFetchedData.api_adress}`);
+              const downConsole = new EmbedBuilder()
+                .setColor(parseInt("E9D84E", 16))
+                .setTitle(`API is down !`)
+                .setDescription(
+                  `Find here log for latest ping\r\n\`\`\`An error occured on API ping for ${apiFetchedData.api_adress} → ${error.response.status} [${error.message}]\`\`\``
+                );
+              await threadPing.send({
+                content: `<@&${apiFetchedData.role ? apiFetchedData.role : role}>`,
+                embeds: [pingInit, downConsole],
+              });
+            } catch (error) {
+              logger(`🔴 [ping:create_send_thread] ${error}`);
+            }
+
+            lastApiState = parseInt(lastApiState) - 1;
+          }
         }
-      );
-    } catch (error) {
-      logger(`🔴 [ping:setinterval] ${error}`);
-    }
+      } catch (error) {
+        // this api has removed from watching
+        logger(`🔴 [ping:inactive_ping] ${error}`);
+        clearInterval(pingInterval);
+      }
+    }, waitingTime);
   } catch (error) {
     logger(`🔴 [ping:global] ${error}`);
   }
 };
-
-module.exports = { statyPing };
