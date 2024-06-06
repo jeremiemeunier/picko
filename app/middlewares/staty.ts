c3f6287ae9598cb13c48dd80466cca4297eda36e
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-06 18:14:24 +0200

    Update staty.ts

--- app/middlewares/staty.ts
+++ app/middlewares/staty.ts
@@ -1,14 +1,4 @@
-export const staty = async (
-  req: { headers: { statyid: string | undefined } },
-  res: {
-    status: (arg0: number) => {
-      (): any;
-      new (): any;
-      json: { (arg0: { message: string }): void; new (): any };
-    };
-  },
-  next: () => void
-) => {
+export const staty = async (req: any, res: any, next: () => void) => {
   if (req.headers.statyid === process.env.BOT_ID) {
     next();
   } else {
