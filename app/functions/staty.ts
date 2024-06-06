b21f52c6c847f0ed910cc82d986612cd2b2a2b87
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-06 18:03:15 +0200

    Create staty.ts

---
+++ app/functions/staty.ts
@@ -0,0 +1,73 @@
+import { Guild } from "discord.js";
+import StatyAxios from "../libs/StatyAxios";
+import logs, { composeTime } from "./logs";
+import { testing } from "./tester";
+
+export const staty = async (guild: Guild) => {
+  try {
+    const setup = await StatyAxios.get("/setup", {
+      params: {
+        guild: guild.id,
+      },
+      headers: {
+        statyid: process.env.BOT_ID,
+      },
+    });
+
+    if (setup.data.data) {
+      const { role, channel, waiting } = setup.data.data;
+      const statsChannel: any = guild.channels.cache.find(
+        (statsChannel) => statsChannel.id === channel
+      );
+
+      if (statsChannel) {
+        try {
+          statsChannel.setTopic(`**Started at :** ${composeTime()}`);
+        } catch (error: any) {
+          logs("error", "staty:update_topic_start", error, guild.id);
+        }
+      }
+
+      try {
+        const fetchAllApi = await StatyAxios.get("/api/all", {
+          params: {
+            guild: guild.id,
+          },
+          headers: {
+            statyid: process.env.BOT_ID,
+          },
+        });
+
+        const allGuildApi = fetchAllApi.data.data;
+
+        logs(
+          "success",
+          "staty:starting_ping",
+          "Successfully getting all api pings",
+          guild.id
+        );
+
+        allGuildApi.map(
+          (api: {
+            _id: string;
+            guild_id: String;
+            role: String;
+            api_name: String;
+            api_adress: String;
+          }) => {
+            testing(api, {
+              state: statsChannel,
+              role: role,
+              guild: guild,
+              wait: waiting,
+            });
+          }
+        );
+      } catch (error: any) {
+        logs("error", "staty:starter:launch", error, guild.id);
+      }
+    }
+  } catch (error: any) {
+    logs("error", "staty:get_setup", error, guild.id);
+  }
+};
