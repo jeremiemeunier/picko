8e30f137e791fa7898dcdc131fda2e2359ca9ba5
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-14 15:52:43 +0200

    Update ping.ts

--- app/routes/ping.ts
+++ app/routes/ping.ts
@@ -6,6 +6,34 @@ import logs from "../functions/logs";
 
 const route = Router();
 
+route.get("/ping/extern", staty, async (req, res) => {
+  const { adress, guild, size } = req.query;
+
+  try {
+    const findItem = await Api.findOne({ guild_id: guild, api_adress: adress });
+
+    if (findItem) {
+      try {
+        const findItemList = await Ping.find({ api_id: findItem._id })
+          .sort({ date: "desc" })
+          .limit(typeof size === "number" && size ? size : 90);
+        res.status(200).json({
+          data: findItemList,
+          message: `All ping find for last ${size ? size : 90} days`,
+        });
+      } catch (error: any) {
+        logs("error", "api:route:ping:get:extern", error);
+        res.status(500).json({ message: "Somethings went wrong" });
+      }
+    } else {
+      res.status(404).json({ message: "Item not found" });
+    }
+  } catch (error: any) {
+    logs("error", "api:route:ping:get:extern", error);
+    res.status(500).json({ message: "Somethings went wrong" });
+  }
+});
+
 route.get("/ping/:id", staty, async (req, res) => {
   const { id } = req.params;
   const { size } = req.query;
@@ -28,36 +56,11 @@ route.get("/ping/:id", staty, async (req, res) => {
   }
 });
 
-route.get("/ping/extern", staty, async (req, res) => {
-  const { adress, guild, size } = req.query;
-
-  try {
-    const findItem = await Api.findOne({ guild_id: guild, api_adress: adress });
-
-    if (findItem) {
-      try {
-        const findItemList = await Ping.find({ api_id: findItem._id })
-          .sort({ date: "desc" })
-          .limit(typeof size === "number" && size ? size : 90);
-        res.status(200).json({
-          data: findItemList,
-          message: "All ping find for last 24 hours",
-        });
-      } catch (error: any) {
-        logs("error", "api:route:ping:get:extern", error);
-        res.status(500).json({ message: "Somethings went wrong" });
-      }
-    }
-  } catch (error: any) {
-    logs("error", "api:route:ping:get:extern", error);
-    res.status(500).json({ message: "Somethings went wrong" });
-  }
-});
-
 route.post("/ping/:id", staty, async (req, res) => {
   const { id } = req.params;
-  const { state, date, guild_id } = req.body;
+  const { state, guild_id } = req.body;
 
+  const date = new Date().toString();
   const buildedDate = new Date().toLocaleDateString("fr-FR");
 
   try {
