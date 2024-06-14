88d13fb1d83ef5b43c8bd060da4489c2452524a5
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-14 15:52:30 +0200

    update commands

--- app/commands/base/api.ts
+++ app/commands/base/api.ts
@@ -1,40 +1,38 @@
 export default {
-  data: {
-    name: "api",
-    description: "Gestion of api bind",
-    default_member_permissions: 0,
-    database: false,
-    options: [
-      {
-        name: "add",
-        description: "Configure a role for notifications",
-        type: 1,
-        options: [
-          {
-            name: "name",
-            description: "An api name",
-            type: 3,
-            required: true,
-          },
-          {
-            name: "adress",
-            description: "An api adress",
-            type: 3,
-            required: true,
-          },
-          {
-            name: "role",
-            description: "A role",
-            type: 8,
-            required: false,
-          },
-        ],
-      },
-      {
-        name: "remove",
-        description: "Remove an api of the bind",
-        type: 1,
-      },
-    ],
-  },
+  name: "api",
+  description: "Gestion of api bind",
+  default_member_permissions: 0,
+  database: false,
+  options: [
+    {
+      name: "add",
+      description: "Configure a role for notifications",
+      type: 1,
+      options: [
+        {
+          name: "name",
+          description: "An api name",
+          type: 3,
+          required: true,
+        },
+        {
+          name: "adress",
+          description: "An api adress",
+          type: 3,
+          required: true,
+        },
+        {
+          name: "role",
+          description: "A role",
+          type: 8,
+          required: false,
+        },
+      ],
+    },
+    {
+      name: "remove",
+      description: "Remove an api of the bind",
+      type: 1,
+    },
+  ],
 };
--- app/commands/base/stats.ts
+++ app/commands/base/stats.ts
@@ -1,8 +1,6 @@
 export default {
-  data: {
-    name: "stats",
-    description: "Return statistics of last 24 hours for api",
-    default_member_permissions: 0,
-    database: true,
-  },
+  name: "stats",
+  description: "Return statistics of last 24 hours for api",
+  default_member_permissions: 0,
+  database: true,
 };
