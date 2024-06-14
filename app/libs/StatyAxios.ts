b746d6a7c3787d6f0136dc219a0c4f8f9cf8a967
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-14 15:52:19 +0200

    Update StatyAxios.ts

--- app/libs/StatyAxios.ts
+++ app/libs/StatyAxios.ts
@@ -2,23 +2,16 @@ import axios from "axios";
 
 let urlRequest = "";
 
-if (process.env.DEV === "1") {
-  urlRequest = "http://localhost:4000";
-} else {
-  urlRequest = "http://localhost:3000";
-}
-
 const StatyAxios = axios.create({
-  baseURL: urlRequest,
+  baseURL: "http://localhost:3000",
   headers: {
-    "PooksApi-Key": process.env.API_KEY,
+    Authorization: `Bearer ${process.env.BOT_ID}`,
   },
 });
 
 StatyAxios.interceptors.response.use(
   (response) => response,
   (error) => {
-    console.log(error.response.data);
     return Promise.reject(error.response.data);
   }
 );
