891a9c93c17048927cd27e94142de1afaa0f59b9
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-21 11:16:19 +0200

    Update staty.ts

--- app/middlewares/staty.ts
+++ app/middlewares/staty.ts
@@ -1,18 +1,38 @@
 import logs from "../functions/logs";
+import User from "../models/User";
 
 export const staty = async (req: any, res: any, next: () => void) => {
   const { authorization } = req.headers;
   const { BOT_ID } = process.env;
+  const key = authorization.split(" ")[1];
 
-  try {
-    if (authorization.split(" ")[1] === BOT_ID) {
-      next();
-    } else {
+  if (key) {
+    try {
+      if (key === BOT_ID) {
+        next();
+      } else {
+        try {
+          const findUser = await User.findById({
+            _id: key,
+          });
+
+          if (findUser) {
+            next();
+          } else {
+            res.status(403).json({ message: "Not authorized for this user" });
+          }
+        } catch (error: any) {
+          logs("error", "middleware:find:user", error);
+          logs("error", "middleware:requested_from", req.hostname);
+          res.status(500).json({ message: "An error occured on middleware" });
+        }
+      }
+    } catch (error: any) {
+      logs("error", "middleware", error);
+      logs("error", "middleware:requested_from", req.hostname);
       res.status(403).json({ message: "Not authorized" });
     }
-  } catch (error: any) {
-    logs("error", "middleware", error);
-    logs("error", "middleware:requested_from", req.hostname);
-    res.status(403).json({ message: "Not authorized" });
+  } else {
+    res.status(400).json({ message: "You must indicate a key in headers" });
   }
 };
